dc3dca6ccfc03d10b5c5e5db7990ccc2c67cbbbe
Sivasundararaj <siva.avis47@gmail.com>
2023-10-24 11:18:02 +0530

    Update number.js

--- number.js
+++ number.js
@@ -2,9 +2,9 @@ function addevn(){
 let numb = Number(document.getElementById("inP").value);
 
   if(numb % 2 == 0){
-    document.getElementById("res").innerHTML = "Even";    
+    document.getElementById("res").innerHTML = `The ${numb} is Even`;    
   } else{
-    document.getElementById("res").innerHTML = "odd";
+    document.getElementById("res").innerHTML = `"The ${numb} is odd`;
   }
 
 }
